f9c9aba5d26aec129eafaea16c678b8d848ab2c9
dragonir <dragonirxo@outlook.com>
2022-04-28 08:50:31 +0800

    feat: 交互优化.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,83 +1,98 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-
-var sketch = $('main')[0];
-var sketch_style = getComputedStyle(sketch);
+var sketch = document.getElementById('sketch');
+var sketch_style = window.getComputedStyle(sketch);
 canvas.width = parseInt(sketch_style.getPropertyValue('width'));
 canvas.height = parseInt(sketch_style.getPropertyValue('height'));
 
 var mouse = { x: 0, y: 0 };
 
-canvas.addEventListener('mousemove', function (e) {
-  var c = $('canvas');
-  mouse.x = e.pageX - c.offset().left;
-  mouse.y = e.pageY - c.offset().top;
+canvas.addEventListener('mousemove', e => {
+  mouse.x = e.pageX - canvas.getBoundingClientRect().left;
+  mouse.y = e.pageY - canvas.getBoundingClientRect().top;
 }, false);
-
 
 ctx.lineWidth = 40;
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.strokeStyle = 'black';
 
-canvas.addEventListener('mousedown', function (e) {
+canvas.addEventListener('mousedown', () => {
   ctx.beginPath();
   ctx.moveTo(mouse.x, mouse.y);
   canvas.addEventListener('mousemove', onPaint, false);
 }, false);
 
-canvas.addEventListener('mouseup', function () {
+canvas.addEventListener('mouseup', () => {
   canvas.removeEventListener('mousemove', onPaint, false);
 }, false);
 
-var onPaint = function () {
+var onPaint = () => {
   ctx.lineTo(mouse.x, mouse.y);
   ctx.stroke();
   var url = canvas.toDataURL();
-  $('div').css({ display: 'initial', 'mask-image': 'url(' + url + ')' });
+  document.querySelectorAll('div').forEach(item => {
+    item.style.cssText += `
+      display: initial;
+      -webkit-mask-image: url(${url});
+      mask-image: url(${url});
+    `;
+  });
 };
 
-$('div').css({
-  display: 'initial'
-})
+document.querySelectorAll('div').forEach(item => {
+  item.style.cssText += `
+    display: initial;
+  `;
+});
 
-var timeline = new TimelineMax({ repeat: -1, yoyo: true }),
+var timeline = new TimelineMax({
+  repeat: -1,
+  yoyo: true
+}),
 feTurb = document.querySelector('#heatturb');
 
 timeline.add(
-new TweenMax.to(feTurb, 8, {
-  onUpdate: function () {
-    var bfX = this.progress() * 0.01 + 0.025,
-    bfY = this.progress() * 0.003 + 0.01,
-    bfStr = bfX.toString() + ' ' + bfY.toString();
-    feTurb.setAttribute('baseFrequency', bfStr);
-  } }),
+  new TweenMax.to(feTurb, 8, {
+    onUpdate: function () {
+      var bfX = this.progress() * 0.01 + 0.025,
+        bfY = this.progress() * 0.003 + 0.01,
+        bfStr = bfX.toString() + ' ' + bfY.toString();
+      feTurb.setAttribute('baseFrequency', bfStr);
+    }
+  }),
 0);
 
 
 function clear() {
-  $('div').css({ display: 'none', 'mask-image': 'none' });
+  document.querySelectorAll('div').forEach(item => {
+    item.style.cssText += `
+      display: none;
+      -webkit-mask-image: none;
+      mask-image: none;
+    `;
+  });
 }
 
-$('.button').click(function () {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  clear();
+document.querySelectorAll('.button').forEach(item => {
+  item.addEventListener('click', () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    clear();
+  })
 });
 
-document.getElementById('upload').onchange = function (e) {
+document.getElementById('upload').onchange = function () {
   var imageFile = this.files[0];
   var newImg = window.URL.createObjectURL(imageFile);
-  // var newImg = $("input").value;
-  console.log(newImg);
   clear();
-  $('main').css({
-    'background': 'url("' + newImg + '")',
-    'background-size': 'cover',
-    'background-position': 'center' });
-
-  $('.mask-inner').css({
-    'background': 'url("' + newImg + '")',
-    'background-size': 'cover',
-    'background-position': 'center' });
-
+  document.getElementById('sketch').style.cssText += `
+    background: url(${newImg});
+    background-size: cover;
+    background-position: center;
+  `;
+  document.getElementById('maskInner').style.cssText += `
+    background: url(${newImg});
+    background-size: cover;
+    background-position: center;
+  `;
 };
